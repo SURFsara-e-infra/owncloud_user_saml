9a2a22d9424003cc98c8048ad4511696e8d4f907
Ron Trompert <ron.trompert@surfsara.nl>
2014-03-16 17:58:16 +0100

    put in the setting of random password

--- settings_personal.php
+++ settings_personal.php
@@ -2,14 +2,24 @@
 
 OCP\Util::addscript('user_saml', 'settings_personal');
 
+function randomPassword() {
+    $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+    $pass = array(); //remember to declare $pass as an array
+    $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+    for ($i = 0; $i < 12; $i++) {
+        $n = rand(0, $alphaLength);
+        $pass[] = $alphabet[$n];
+    }
+    return implode($pass); //turn the array into a string
+}
+           
 if ($_POST) {
 	// CSRF check
 	OCP\JSON::callCheck();
 	OCP\JSON::checkLoggedIn();
 
 	$username = \OCP\User::getUser();
-	$password = isset($_POST['password']) ? $_POST['password'] : null;
-	$passwordAgain = isset($_POST['confirm_password']) ? $_POST['confirm_password'] : '';
+        $password = randomPassword();
 	if (!is_null($password)) {
 		if (\OC_User::setPassword($username, $password)) {
 			\OCP\JSON::success();
